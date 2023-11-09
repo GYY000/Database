21ab1f98070ba97a743eef9031dce08eaefea4a5
GhostOnSill <1037967826@qq.com>
2023-11-09 08:39:49 +0800

    修复models重复定义

--- backend/User/models.py
+++ backend/User/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+
 class User(models.Model):
     uid = models.AutoField(primary_key=True)
     user_name = models.CharField(max_length=30, unique=True)
     password = models.CharField(max_length=30)
     register_date = models.DateField(auto_now_add=True)
     profile_photo = models.BinaryField(null=True, blank=True)
+
 
 class Team(models.Model):
     tid = models.AutoField(primary_key=True)
@@ -14,7 +16,7 @@ class Team(models.Model):
     create_date = models.DateField(auto_now_add=True)
     introduction = models.CharField(null=False, max_length=200)
     profile_photo = models.BinaryField()
-    introduction = models.CharField(null=False, max_length=200)
+
 
 class ReUserTeam(models.Model):
     id = models.AutoField(primary_key=True)
@@ -26,6 +28,7 @@ class ReUserTeam(models.Model):
     class Meta:
         unique_together = ('uid', 'tid')
 
+
 class Question(models.Model):
     qid = models.AutoField(primary_key=True)
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -33,6 +36,7 @@ class Question(models.Model):
     content = models.JSONField()
     score = models.FloatField()
     serial_num = models.IntegerField()
+
 
 class QuestionSet(models.Model):
     qsid = models.AutoField(primary_key=True)
@@ -42,12 +46,13 @@ class QuestionSet(models.Model):
     is_public = models.BooleanField(null=False)
     introduction = models.CharField(null=False, max_length=200)
     profile_photo = models.BinaryField(null=True, blank=True)
-    introduction = models.CharField(null=False, max_length=200)
+
 
 class QuestionSetPerm(models.Model):
     id = models.AutoField(primary_key=True)
     qsid = models.ForeignKey(QuestionSet, on_delete=models.CASCADE)
     tid = models.ForeignKey(Team, on_delete=models.CASCADE)
+
 
 class Exam(models.Model):
     eid = models.AutoField(primary_key=True)
@@ -57,6 +62,7 @@ class Exam(models.Model):
     start_time = models.DateTimeField()
     duration = models.DurationField()
 
+
 class SetHistory(models.Model):
     shid = models.AutoField(primary_key=True)
     uid = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -64,14 +70,17 @@ class SetHistory(models.Model):
     time = models.DateTimeField(auto_now_add=True)
     score = models.FloatField()
 
+
 class QuestionHistory(models.Model):
     qhid = models.AutoField(primary_key=True)
     shid = models.ForeignKey(SetHistory, on_delete=models.CASCADE)
     answer = models.CharField(max_length=400)
     score = models.FloatField()
 
+
 class SensitiveWord(models.Model):
     word = models.CharField(max_length=30, primary_key=True)
+
 
 class Message(models.Model):
     id = models.AutoField(primary_key=True)
@@ -81,14 +90,16 @@ class Message(models.Model):
     content = models.CharField(max_length=300)
     read = models.BooleanField(default=False)
 
+
 class Post(models.Model):
     pid = models.AutoField(primary_key=True)
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
-    #fixme auto_now_add -> auto_now
+    # fixme auto_now_add -> auto_now
     title = models.CharField(max_length=40)
+
 
 class Comment(models.Model):
     id = models.AutoField(primary_key=True)
@@ -96,6 +107,7 @@ class Comment(models.Model):
     content = models.TextField()
     create_time = models.DateTimeField(auto_now_add=True)
     pid = models.ForeignKey(Post, on_delete=models.CASCADE)
+
 
 class JoinRequest(models.Model):
     id = models.AutoField(primary_key=True)
